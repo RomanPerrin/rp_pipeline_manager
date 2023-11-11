ee9cd93a21a088053ef0e721c1b151a10dd29133
RomanPerrin <romanperrin73@gmail.com>
2023-11-11 21:46:45 +0100

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -101,7 +101,6 @@ class UI():
         cmds.showWindow(self.window)
     
     def update(self, *args):
-        reload(install)
         install.updater()
         cmds.deleteUI(self.window)
         import rp_pipeline_manager
