9c6a8dde30e3626b9862afbb34d8de20b11c5e22
RomanPerrin <138393628+RomanPerrin@users.noreply.github.com>
2023-12-04 16:57:28 +0100

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -288,7 +288,7 @@ class UI():
         return
     
     def publish(self, *args):
-        selection_export = cmds.ls(sl=1)
+        selection_export = cmds.ls(sl=1, dag=1)
         if not selection_export:
             dismissed = cmds.framelessDialog( title='Publish error', message='No selection found', 
                     path='please select an item and try again', button=['OK'], primary=['OK'])
@@ -368,7 +368,8 @@ class UI():
                     cmds.delete(shape)
                     print("deleting", shape, "namespace")
 
-            cmds.select(selection_export)
+			shadingGrps = cmds.listConnections(selection_export,type='shadingEngine')
+			cmds.select(shadingGrps + selection_export, noExpand=True)
             cmds.file(file_name, force = True, options = "v=0", type = "mayaAscii", shader = True, constructionHistory = True, exportSelected = True) 
             print("publish scene saved")
 
