e19a3691010a2208b6064fd66b4a385c261de47e
RomanPerrin <romanperrin73@gmail.com>
2024-03-09 19:12:51 +0100

    Update sceneUtility.py

--- sceneUtility.py
+++ sceneUtility.py
@@ -27,7 +27,9 @@ def openScene(dir, *args):
 
 def readSetting(setting):
     file = os.path.join(sys.modules[__package__].__path__[0], 'settings.json')
+    print(file)
     content = json.loads(file)
+    print(content)
     print(content[setting])
 
 
