5df75458af432ffe7d7a6dd6dc35a929cf53e743
RomanPerrin <romanperrin73@gmail.com>
2024-03-09 18:07:02 +0100

    setting file

--- sceneUtility.py
+++ sceneUtility.py
@@ -3,12 +3,15 @@ __author__ = 'Roman PERRIN'
 #Author: Roman PERRIN
 
 #Libraries
+import json
 import maya.cmds as cmds
 import maya.mel as mel
 import os
 
 #files
 from . import pluginUtility
+
+print(__package__)
 
 def saveScene(*args):
     if cmds.file(q=True, sceneName=True):
@@ -23,3 +26,7 @@ def openScene(dir, *args):
     if os.path.exists(dir):
         opened_file = cmds.file(dir, open=True , force=True)
         return
+
+def readSetting(setting):
+    os.path.join(__package__)
+    # json.loads()
