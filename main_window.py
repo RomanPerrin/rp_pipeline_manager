651f07b533845627dd8b69f591e95dcae9b87b22
RomanPerrin <romanperrin73@gmail.com>
2024-03-12 15:37:04 +0100

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -54,6 +54,7 @@ class UI():
         menu = cmds.menu(l='Tools', p=menuBarLayout)
         cmds.menuItem(l='Change Cameras Clip Plane', p=menu, c=self.changeCamerasClipPlane)
         cmds.menuItem(l='Auto Instancer', p=menu, c=instancer.duplicateSpecialInstancer)
+        cmds.menuItem(l='Auto Instancer', p=menu, c=instancer.autoInstance)
         cmds.menuItem(l='create set instance for current selection', p=menu, c=self.createSetInstance)
         menu = cmds.menu(l='About', p=menuBarLayout)
         cmds.menuItem(l='Update', p=menu, c=self.update)
